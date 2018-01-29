248541e82f5cb4fb590667ad6d95a3f7361801bf
ZC-H <cern.hoh@gmail.com>
2018-01-29 17:20:10 +0800

    Users can sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,27 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = user.id
 			redirect_to '/'
 		else
 			p @user.errors
 			flash[:danger] = @user.errors
 			redirect_to '/sign_up'
+		end
+	end
+
+	def sign_in
+	end
+
+	def sign_in_check
+		p "AAAAAAAAAAAAAAAAAAA"
+		p params
+		id = User.login(params)
+		if id
+			session[:user_id] = id
+			redirect_to '/'
+		else
+			redirect '/sign_in'
 		end
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   get "/sign_up" => "users#new", as: "sign_in"
-  
+  get "/sign_in" => "users#sign_in"
+  post "/sign_in" => "users#sign_in_check"
 end
